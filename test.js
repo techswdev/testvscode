7a4059112344da12f196bb0788450d81414f59c8
techinha221 <techinha221@naver.com>
2021-07-16 10:55:10 +0900

    fourth only aa3

--- test.js
+++ test.js
@@ -2,3 +2,8 @@ function add(a,b,c){
     console.log("🚀 ~ file: test.js ~ line 2 ~ add ~ a", a)
 }
 add(1,2,3);
+
+function add1(a,b,c){
+    console.log("🚀 ~ file: test.js ~ line 2 ~ add ~ a", a)
+}
+add(1,2,3);
